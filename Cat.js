74099afa7506ffc7fdcd80d865ff7b5bb55730da
Sucy <edgcuong@gmail.com>
2020-04-23 09:07:45 +0700

    Add doge

--- Cat.js
+++ Cat.js
@@ -1,5 +1,9 @@
 function Cat(){
-    thís.stomach = [];
+    this.stomach = [];
+}
+
+Cat.prototype.eat = (mouse)=>{
+     this.stomach.push(mouse);
 }
 
 module.exports = Cat;
---
+++ Dog.js
@@ -0,0 +1,6 @@
+function Dog(name){
+    this.name = name;
+}
+
+
+module.exports = Dog;
